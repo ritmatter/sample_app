813c82e9d8b1339edb2c15cde2881c1a41e691f6
ritmatter <mritter123@gmail.com>
2013-12-02 01:01:16 -0500

    clean up tests for passwords

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,16 @@ describe User do
 
   it { should be_valid }
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.com" }
+    
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
   describe "when password is not present" do
     before do
       @user = User.new(name: "Example User", email: "user@example.com",
@@ -69,7 +79,7 @@ describe User do
 
   describe "when email format is invalid" do
     it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar_baz_.com foo@bar+bazcom]
+      addresses = %w[user@foo..com user@foo,com user_at_foo.org example.user@foo.foo@bar_baz_.com foo@bar+bazcom]
       addresses.each do |invalid_address|
         @user.email = invalid_address
 	expect(@user).not_to be_valid
