791b9dc9b87856b2b51ec84c34d0f3d5bd4e6add
ritmatter <mritter123@gmail.com>
2013-12-12 23:41:59 -0500

    finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,7 +10,7 @@ describe "User pages" do
     it { should have_title('Sign up') }
     it { should have_title(full_title('Sign up')) }
   end
-	   
+     
   describe "signup" do
     before { visit signup_path }
 
@@ -23,18 +23,18 @@ describe "User pages" do
 
       describe "after submission" do
         before { click_button submit }
-	  
-	it { should have_title('Sign up') }
-	it { should have_content('error') }
+    
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
       end
     end
 
     describe "with valid information" do
       before do
-        fill_in "Name",			with: "Example User"
-	fill_in "Email", 		with: "user@example.com"
-	fill_in "Password", 		with: "foobar"
-	fill_in "Confirmation", 	with: "foobar"
+        fill_in "Name",     with: "Example User"
+        fill_in "Email",    with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation",   with: "foobar"
       end
 
       it "should create a user" do
@@ -43,19 +43,28 @@ describe "User pages" do
 
       describe "after saving the user" do
         before { click_button submit }
-	let(:user) { User.find_by(email: 'user@example.com') }
+        let(:user) { User.find_by(email: 'user@example.com') }
 
-	it { should have_title(user.name) }
-	it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: "user@example.com") }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
-  end
 
-  describe "profile page" do
-    let(:user) { FactoryGirl.create(:user) }    
-    before { visit user_path(user) }
+    describe "profile page" do
+      let(:user) { FactoryGirl.create(:user) }    
+      before { visit user_path(user) }
     
-    it { should have_content(user.name) }
-    it { should have_title(user.name) }
+      it { should have_content(user.name) }
+      it { should have_title(user.name) }
+    end
   end
 end
